073cdbdc578cf29026e4d99bc8048f1aaa742b76
Gustavo Alexandre Dias <diasgustavo2000@hotmail.com>
2024-04-06 02:16:54 -0300

    Sincronizando o banco de dados

---
+++ .history/src/model/event_20240406020221.js
@@ -0,0 +1,85 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const { allow } = require('joi');
+const moment = require('moment')
+
+//todo Mudar o nome da tabela la na modelagem para Event
+//todo Caso for necessario eh possivel inserir novos campos
+
+/*
+
+    Cod_Event
+    Rua
+    nome
+    Numero
+    Cidade
+    Preoco
+
+*/
+
+
+const Event = sequelize.define('Event', {
+
+    cod_Event: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        defaultValue: "John Doe",
+        allowNull: true
+    },
+    rua: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    numero: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    cidade: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    preco: {
+        type: DataTypes.DECIMAL,
+        allowNull: true
+    },
+    data: { // Analisar como ficou
+        type: DataTypes.DATE,
+        field: 'created_at',
+        allowNull: false
+    },
+
+    testDate: {
+        type: DataTypes.DATE,
+        defaultValue: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
+        defaultValue: DataTypes.NOW,
+        allowNull: true
+        // This way, the current date/time will be used to populate this column (at the moment of insertion)
+    },
+
+    myDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW, allowNull: false },
+
+}, {
+    freezeTableName: true,
+    timestamps: true
+}
+
+);
+
+
+console.log('Tabela Event');
+console.log(sequelize.models.Event);
+
+
+
+/*
+  createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
+      field: 'created_at'
+    },
+*/
