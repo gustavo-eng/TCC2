26a762e31490660ec05bb328fff0d6fa5e1475c1
Gustavo Alexandre Dias <diasgustavo2000@hotmail.com>
2024-05-15 23:54:04 -0300

    Desenvolvimento da api de address

---
+++ server/.history/src/controllers/addressController_20240515234911.js
@@ -0,0 +1,19 @@
+
+const db = require('../config/db');
+const Address = db.Address;
+
+
+const { success, fail, message } = require('../helpers/response');
+
+
+exports.findAll = async (req, res) => {
+    await Address.findAll().then(address => {
+
+    })
+}
+
+
+//create
+//findAll
+//update
+//delete - tratar caso buscar e nao conter address correspondente
