265bfd41f89e90f09eeb41302bde706d175b840f
Gustavo Alexandre Dias <diasgustavo2000@hotmail.com>
2024-06-23 15:26:40 -0300

    :construction: Developing reset password

--- server/.history/src/routes/resetPasswordAPI_20240623151653.js
+++ server/.history/src/routes/resetPasswordAPI_20240623151653.js
@@ -2,18 +2,18 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 require('dotenv').config();
-
 router.post("/", async (req, res) => {
 
     let transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
             user: "gustavodias2609200@gmail.com",
-            pass: process.env.PASS_EMAIL, // todo analisar troca dinamica
+            pass: "twsgiexgerjhcjju", // todo analisar troca dinamica
         },
         tls: {//todo analisar este tls
             rejectUnauthorized: false // Ignorar verificação do certificado SSL
         }
+
     });
 
     let mailOptions = {
--- server/.history/src/routes/resetPasswordAPI_20240623151755.js
+++ server/.history/src/routes/resetPasswordAPI_20240623151755.js
@@ -14,6 +14,7 @@ router.post("/", async (req, res) => {
         tls: {//todo analisar este tls
             rejectUnauthorized: false // Ignorar verificação do certificado SSL
         }
+
     });
 
     let mailOptions = {
